32cb401b2ce2f47ddf366e72fae391cee1b64e0f
ndownes <ndownes765@gmail.com>
2017-04-21 08:02:45 +1000

    Update entertainment_centre.py

--- entertainment_centre.py
+++ entertainment_centre.py
@@ -6,7 +6,7 @@ import media
 the_informant = media.Movie("THE INFORMANT",
                             "(2009)",
                             "79% Rotten Tomatoes",
-                            "'Matt Damon is a delusional but brilliant"
+                            "'Matt Damon is a delusional but brilliant "
                             "white-collar criminal turned FBI mole!'",
                             "Paul Asay, Plugged In",
                             "https://upload.wikimedia.org/wikipedia/en/3/3c/TheInformant2009MP.jpg",  # NOQA
@@ -15,7 +15,7 @@ the_informant = media.Movie("THE INFORMANT",
 oceans_eleven = media.Movie("OCEAN'S ELEVEN",
                             "(2001)",
                             "82% Rotten Tomatoes",
-                            "'Ocean's 11 is good old-fashioned Hollywood"
+                            "'Ocean's 11 is good old-fashioned Hollywood "
                             "heist film fun.'",
                             "Nell Minow, Common Sense Media",
                             "https://upload.wikimedia.org/wikipedia/en/6/68/Ocean%27s_Eleven_2001_Poster.jpg",  # NOQA
@@ -24,8 +24,8 @@ oceans_eleven = media.Movie("OCEAN'S ELEVEN",
 chicago = media.Movie("CHICAGO",
                       "(2002)",
                       "86% Rotten Tomatoes",
-                      "Chicago dazzles audiences with colour, action, and"
-                      "rip-roaring sound.",
+                      "Chicago dazzles audiences with colour, action, "
+                      "and rip-roaring sound.",
                       "Lindy Keffer, Plugged In",
                       "https://upload.wikimedia.org/wikipedia/en/e/e0/Chicagopostercast.jpg",  # NOQA
                       "https://www.youtube.com/watch?v=9EpaMmF9WVU")
@@ -33,7 +33,7 @@ chicago = media.Movie("CHICAGO",
 limitless = media.Movie("LIMITLESS",
                         "(2011)",
                         "70% Rotten Tomatoes",
-                        "'A loopy joy from start to finish, Bradley Cooper"
+                        "'A loopy joy from start to finish, Bradley Cooper "
                         "proves that he's the real deal.'",
                         "David Hughes, Empire",
                         "https://upload.wikimedia.org/wikipedia/en/1/17/Limitless_Poster.jpg",  # NOQA
@@ -42,7 +42,7 @@ limitless = media.Movie("LIMITLESS",
 the_imitation_game = media.Movie("THE IMITATION GAME",
                                  "(2014)",
                                  "90% Rotten Tomatoes",
-                                 "'A crackling spy thriller dished out by"
+                                 "'A crackling spy thriller dished out by "
                                  "experts.'",
                                  "Peter Travers, Rolling Stone",
                                  "https://upload.wikimedia.org/wikipedia/en/5/5e/The_Imitation_Game_poster.jpg",  # NOQA
@@ -51,7 +51,7 @@ the_imitation_game = media.Movie("THE IMITATION GAME",
 liar_liar = media.Movie("LIAR LIAR",
                         "(1997)",
                         "81% Rotten Tomatoes",
-                        "'I am gradually developing a suspicion that Jim"
+                        "'I am gradually developing a suspicion that Jim "
                         "Carrey is growing on me.'",
                         "Roger Ebert",
                         "https://upload.wikimedia.org/wikipedia/en/3/3d/Liar_Liar_poster.JPG",  # NOQA
@@ -68,7 +68,7 @@ duplicity = media.Movie("DUPLICITY",
 his_girl_friday = media.Movie("HIS GIRL FRIDAY",
                               "(1940)",
                               "98% Rotten Tomatoes",
-                              "'A fast-talking screwball delight that"
+                              "'A fast-talking screwball delight that "
                               "remains the finest example of its genre.'",
                               "Colin Kennedy, Empire",
                               "https://upload.wikimedia.org/wikipedia/en/1/1a/His_Girl_Friday_poster.jpg",  # NOQA
@@ -77,7 +77,7 @@ his_girl_friday = media.Movie("HIS GIRL FRIDAY",
 twentyone_jump_street = media.Movie("21 JUMP STREET",
                                   "(2012)",
                                   "85% Rotten Tomatoes",
-                                  "'A smart, affectionate satire of '80s"
+                                  "'A smart, affectionate satire of '80s "
                                   "nostalgia and teen movie tropes.'",
                                   "Critics' consensus, Rotten Tomatoes",
                                   "https://upload.wikimedia.org/wikipedia/en/9/93/21JumpStreetfilm.jpg",  # NOQA
@@ -86,18 +86,18 @@ twentyone_jump_street = media.Movie("21 JUMP STREET",
 the_social_network = media.Movie("THE SOCIAL NETWORK",
                                "(2010)",
                                "96% Rotten Tomatoes",
-                               "'The tale of how Mark Zuckerberg hit on the"
-                               "idea of Facebook is a riveting movie about"
-                               "friendship and greed.'",
+                               "'The tale of how Mark Zuckerberg hit on "
+                               "the idea of Facebook is a riveting movie "
+                               "about friendship and greed.'",
                                "The Guardian",
                                "https://upload.wikimedia.org/wikipedia/en/7/7a/Social_network_film_poster.jpg",  # NOQA
-                               "https://www.youtube.com/watch?v=lB95KLmpLR4")
+                               "https://www.youtube.com/watch?v=lB95KLmpLR4")  # NOQA
 
 the_big_short = media.Movie("THE BIG SHORT",
                             "(2015)",
                             "88% Rotten Tomatoes",
-                            "'Witty, absurd and far more entertaining than"
-                            "it has any right to be.'",
+                            "'Witty, absurd and far more entertaining "
+                            "than it has any right to be.'",
                             "Helen O'Hara, Empire",
                             "https://upload.wikimedia.org/wikipedia/en/e/e3/The_Big_Short_teaser_poster.jpg",  # NOQA
                             "https://www.youtube.com/watch?v=vgqG3ITMv1Q")
@@ -113,7 +113,7 @@ the_devil_wears_prada = media.Movie("THE DEVIL WEARS PRADA",
 unbroken = media.Movie("UNBROKEN",
                      "(2014)",
                      "51% Rotten Tomatoes",
-                     "'A movie about forgiveness and enduring the"
+                     "'A movie about forgiveness and enduring the "
                      "unendurable.'",
                      "Rotten Tomatoes",
                      "https://upload.wikimedia.org/wikipedia/en/7/76/Unbroken_poster.jpg",  # NOQA
@@ -122,8 +122,8 @@ unbroken = media.Movie("UNBROKEN",
 catch_me_if_you_can = media.Movie("CATCH ME IF YOU CAN",
                                   "(2002)",
                                   "96% Rotten Tomatoes",
-                                  "'Frank Abagnale not only forges cheques,"
-                                  "he forges lives.'",
+                                  "'Frank Abagnale not only forges "
+                                  "cheques, he forges lives.'",
                                   "Loren Eaton, Plugged In",
                                   "https://upload.wikimedia.org/wikipedia/en/4/4d/Catch_Me_If_You_Can_2002_movie.jpg",  # NOQA
                                   "https://www.youtube.com/watch?v=71rDQ7z4eFg")  # NOQA
@@ -134,21 +134,22 @@ charlies_angels = media.Movie("CHARLIES ANGELS",
                             "'Edgy girl power fun.'",
                             "Nell Minow, Common Sense Media",
                             "https://upload.wikimedia.org/wikipedia/en/a/a6/Charlies_Angels_%282000%29_Poster.jpg",  # NOQA
-                            "https://www.youtube.com/watch?v=rRHRa80wVq8")
+                            "https://www.youtube.com/watch?v=rRHRa80wVq8")  # NOQA
 
 get_smart = media.Movie("GET SMART",
                         "(2008)",
                         "51% Rotten Tomatoes",
-                        "'You could quibble about the movie's uneven plot,"
-                        "but don't let that get in the way of a good time.'",
+                        "'You could quibble about the movie's uneven "
+                        "plot, but don't let that get in the way of "
+                        "a good time.'",
                         "S. Jhoanna Robledo, Common Sense Media",
                         "https://upload.wikimedia.org/wikipedia/en/8/88/Getsmart08.png",  # NOQA
                         "https://www.youtube.com/watch?v=K9WNBO3szgQ")
 
 # Lists all movies displayed on page
-movies = [the_informant, oceans_eleven, chicago, limitless, the_imitation_game,
-          liar_liar, duplicity, his_girl_friday, the_big_short,
-          the_devil_wears_prada, unbroken, catch_me_if_you_can,
-          charlies_angels, get_smart, twentyone_jump_street,
-          the_social_network]
+movies = [the_informant, oceans_eleven, chicago, limitless,
+          the_imitation_game, liar_liar, duplicity, his_girl_friday,
+          the_big_short, the_devil_wears_prada, unbroken,
+          catch_me_if_you_can, charlies_angels, get_smart,
+          twentyone_jump_street, the_social_network]
 fresh_tomatoes.open_movies_page(movies)
